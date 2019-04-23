4f19450cbcda254d111a5df0e908bdb99cdbf225
cc0a <jmckeeco84@gmail.com>
2019-04-23 19:08:08 -0400

    404 page added

--- src/pages/Default.js
+++ src/pages/Default.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
-
+import Header from '../components/Header';
+import {Link} from 'react-router-dom';
 export default class Default extends Component {
     render() {
-        return (
-            <h4>
-                Hello from Default Page!
-            </h4>
-        );
+        return <Header title='404' styleClass='default-hero'>
+            <h2 className="text-light text-uppercase">Page Does Not Exist</h2>
+            <Link
+                to='/'
+                className="text-uppercase btn btn-secondary btn-large mt-3"
+            >
+                Return Home
+            </Link>
+        </Header>;
     }
 }
