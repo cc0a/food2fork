d5105c42d9f326b95ea48c2edae3845be2237e8a
cc0a <jmckeeco84@gmail.com>
2019-04-24 15:18:24 -0400

    1.0 front-end

--- src/pages/Default.js
+++ src/pages/Default.js
@@ -1,16 +1,17 @@
-import React, {Component} from 'react';
-import Header from '../components/Header';
-import {Link} from 'react-router-dom';
+import React, { Component } from "react";
+import Header from "../components/Header";
+import { Link } from "react-router-dom";
 export default class Default extends Component {
-    render() {
-        return <Header title='404' styleClass='default-hero'>
-            <h2 className="text-light text-uppercase">Page Does Not Exist</h2>
-            <Link
-                to='/'
-                className="text-uppercase btn btn-secondary btn-large mt-3"
-            >
-                Return Home
-            </Link>
-        </Header>;
-    }
+  render() {
+    return (
+      <Header title="404" styleClass="default-hero">
+        <h2 className="text-light text-uppercase">
+          you are in the wrong place
+        </h2>
+        <Link to="/" className="text-uppercase btn btn-secondary btn-lg mt-3">
+          return home
+        </Link>
+      </Header>
+    );
+  }
 }
--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,27 +1,87 @@
-import React, {Component} from 'react';
-import RecipeList from '../components/RecipeList'
-import Search from '../components/Search'
-import {recipeData} from '../data/tempList';
+import React, { Component } from "react";
+import RecipeList from "../components/RecipeList";
+import Search from "../components/Search";
+import { recipeData } from "../data/tempList";
 export default class Recipes extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
+    this.getRecipes = this.getRecipes.bind(this);
+  }
+  state = {
+    recipes: recipeData,
+    search: "",
+    url: `https://www.food2fork.com/api/search?key=${
+      process.env.REACT_APP_API_KEY
+    }`,
+    base_url: `https://www.food2fork.com/api/search?key=${
+      process.env.REACT_APP_API_KEY
+    }`,
+    query: "&q=",
+    error: ""
+  };
+
+  async getRecipes() {
+    try {
+      const data = await fetch(this.state.url);
+      const jsonData = await data.json();
+      if (jsonData.recipes.length === 0) {
+        this.setState({
+          error:
+            "sorry but your search did not return any recipes, please try again or press search icon for the most popular recipes"
+        });
+      } else {
+        this.setState({
+          recipes: jsonData.recipes,
+          error: ""
+        });
+      }
+    } catch (error) {
+      console.log(error);
     }
-    state={
-        recipes:recipeData,
-        search: ''
-    };
-    handleChange = (e) => {
-       this.setState({
-           search: e.target.value
-       });
-    };
-    handleSubmit = (e) => {
-      e.preventDefault()
-    };
-    render() {
-        return <>
-            <Search search={this.state.search} handleChange={this.handleChange} handleSubmit={this.handleSubmit}/>
-            <RecipeList recipes={this.state.recipes} />
-        </>
-    }
+  }
+
+  componentDidMount() {
+    this.getRecipes();
+  }
+
+  handleChange = e => {
+    this.setState({
+      search: e.target.value
+    });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    const { base_url, query, search } = this.state;
+    this.setState(
+      {
+        url: `${base_url}${query}${search}`,
+        search: ""
+      },
+      () => this.getRecipes()
+    );
+  };
+  render() {
+    return (
+      <>
+        <Search
+          search={this.state.search}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+        />
+        {this.state.error ? (
+          <section>
+            <div className="row">
+              <div className="col">
+                <h2 className="text-orange text-center text-uppercase mt-5">
+                  {this.state.error}
+                </h2>
+              </div>
+            </div>
+          </section>
+        ) : (
+          <RecipeList recipes={this.state.recipes} />
+        )}
+      </>
+    );
+  }
 }
