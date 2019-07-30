b59603e971e52bc76a091b107b6df036c7ddec58
cc0a <jmckeeco84@gmail.com>
2019-07-30 12:35:06 -0600

    updates

--- src/pages/SingleRecipe.js
+++ src/pages/SingleRecipe.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { recipeData } from "../data/tempDetails";
 import { Link } from "react-router-dom";
 export default class SingleRecipe extends Component {
   constructor(props) {
